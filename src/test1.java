e5af40e03dc98bb52978777a1d8d8ea330ea6ee3
Nelly Lapina <nlapina@serena.com>
2012-12-06 15:45:45 +0200

    test1 commit

---
+++ src/test1.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: nlapina
+ * Date: 12/6/12
+ * Time: 3:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class test1 {
+}
